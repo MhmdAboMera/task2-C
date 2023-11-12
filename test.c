de6fa00e0928b316b38869e9c3344462f1906c7c
Your Name <you@example.com>
2023-11-12 21:52:16 +0200

    upload task files

---
+++ test.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    char username[30];
+    char password[30];
+} User;
+
+char number[30];
+#define MAX_USERS 100
+#define MAX_FLAG 10
+
+User users[MAX_USERS];
+int userCount = 0;
+void registerUser()
+{
+    if (userCount == MAX_USERS)
+    {
+        printf("Maximum user limit exceeded. Cannot register more users.\n");
+        return;
+    }
+    User newUser;
+    printf("Enter username: ");
+    scanf("%s", newUser.username);
+
+    printf("Enter password: ");
+    scanf("%s", newUser.password);
+
+    printf("if you active enter 1 or 'true' else enter 0 or 'false' : ");
+    scanf("%s", number);
+
+    users[userCount++] = newUser;
+    printf("User registered successfully!\n");
+}
+
+void loginUser()
+{
+    if ((strcmp(number, "1") == 0) || (strcmp(number, "true") == 0))
+    {
+        char username[30], password[30];
+        printf("Enter username: ");
+        scanf("%s", username);
+
+        printf("Enter password: ");
+        scanf("%s", password);
+        int i = 0;
+        for (i; i < userCount; i++)
+        {
+            if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0)
+            {
+                printf("User logged in successfully!\n");
+                return;
+            }
+        }
+
+        printf("Invalid username or password. Please try again.\n");
+    }
+    else
+    {
+        printf("You Should Be Active First To Login\n ");
+    }
+}
+
+int main()
+{
+    int option;
+    while (1)
+    {
+        printf("Press '1' to Register \nPress '2' to Login \nPress '3' to Exit\n\n");
+        scanf("%d", &option);
+        if (option == 1)
+        {
+            registerUser();
+        }
+        else if (option == 3)
+        {
+            printf("Program terminated.\n");
+            break;
+        }
+        else if (option == 2)
+        {
+            loginUser();
+        }
+        else
+        {
+            printf("Invalid option. Please try again.\n");
+        }
+
+        printf("\n");
+    }
+}
